ec44f23ca4dd07faf0b09c2b10050902875c0c0e
Justin Phillips <euangelion3o9@gmail.com>
2017-11-06 15:52:34 -0500

    Refactored init

--- src/app.py
+++ src/app.py
@@ -1,18 +1,20 @@
 from flask import Flask
-from .controllers.object_api import object
+from .controllers.node_api import node
 from .controllers.edge_api import edge
 
 # initialize the flask app
 app = Flask(__name__)
 
 # register the blueprints from controllers
-app.register_blueprint(object)
+app.register_blueprint(node)
 app.register_blueprint(edge)
+
 
 # setup default route
 @app.route("/")
 def hello():
     return "Hello World!"
+
 
 # "main" function
 if __name__ == "__main__":
